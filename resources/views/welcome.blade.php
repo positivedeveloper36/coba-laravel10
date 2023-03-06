1e3133c9614f1cede36d4aeaa4f130f8de7643ee
Happy Engineer <happyengineer63@gmail.com>
2023-03-06 16:47:56 +0800

    mengubah judul website 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Happy and Enjoy</title>
+        <title>Laravel Happy Development</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
